bc161d3b22757434a03e2425c84cdeab17e111c9
Hoogenboom <wes.hoogenboom@gmail.com>
2018-10-31 11:56:00 -0400

    Adding some boilerplate stuff

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'svh-web.auth0.com',
+        clientId: '_jDrlgOtIPkora0gaVeKtfBGbffKlo4Y'
+    },
+    apiBaseUrl: 'https://yjx32jyyca.execute-api.us-east-1.amazonaws.com/dev'
+};
