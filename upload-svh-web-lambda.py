653212a38252fb880a38e8c2795cec7a6ea49b76
Hoogenboom <wes.hoogenboom@gmail.com>
2018-11-02 14:16:34 -0400

    Use SNS to notify when portfolio is deployed

--- upload-svh-web-lambda.py
+++ upload-svh-web-lambda.py
@@ -6,19 +6,29 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:539127747096:deploySvhWeb')
 
-    prod_bucket = s3.Bucket('svhproductions.com')
-    build_bucket = s3.Bucket('build.svhproductions.com')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    svhbuild_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('svhbuild.zip', svhbuild_zip)
+        prod_bucket = s3.Bucket('svhproductions.com')
+        build_bucket = s3.Bucket('build.svhproductions.com')
 
-    with zipfile.ZipFile(svhbuild_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            prod_bucket.upload_fileobj(obj, nm,
-             ExtraArgs={'ContentType':  mimetypes.guess_type(nm)[0]})
-            prod_bucket.Object(nm).Acl().put(ACL='public-read')
+        svhbuild_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('svhbuild.zip', svhbuild_zip)
+
+        with zipfile.ZipFile(svhbuild_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                prod_bucket.upload_fileobj(obj, nm,
+                 ExtraArgs={'ContentType':  mimetypes.guess_type(nm)[0]})
+                prod_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job Done'
+        topic.publish(Subject="SVH Web Deployed", Message="SVH Web deployed successfully")
+    except:
+        topic.publish(Subject="SVH Web Deploy Failed", Message="SVH Web was not deployed successfully")
+        raise
 
     return 'Hello from Lambda'
